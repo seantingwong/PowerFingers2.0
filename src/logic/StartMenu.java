eb2e7413c5087a1a0ca84f0ffb7ae8cecc0e4a48
Sean Wong <phrrmw@gmail.com>
2018-05-24 15:09:03 -0700

    main menu

--- src/logic/StartMenu.java
+++ src/logic/StartMenu.java
@@ -4,8 +4,12 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 
 public class StartMenu extends Application{
@@ -24,9 +28,15 @@ public class StartMenu extends Application{
 	public void start(Stage stage) throws Exception {
 		stage.setTitle("Power Fingers");
 		GridPane grid = new GridPane();
+		VBox root = new VBox();
+		root.setSpacing(10);
+		root.setPadding(new Insets(0, 20, 10, 20)); 
+		
+		grid.setAlignment(Pos.CENTER);
 		grid.setHgap(5);
 		grid.setVgap(5);
 		grid.setPadding(new Insets(25, 25, 25, 25));
+		grid.getColumnConstraints().add(new ColumnConstraints(150));
 		
 		title = new Label("Power Fingers");
 		play = new Button("Play");
@@ -34,13 +44,26 @@ public class StartMenu extends Application{
 		settings = new Button("Settings");
 		help = new Button("Help");
 
+		title.setMaxWidth(100);
+		play.setMaxWidth(100);
+		scores.setMaxWidth(100);
+		settings.setMaxWidth(100);
+		help.setMaxWidth(100);
+
+		GridPane.setHalignment(title, HPos.CENTER);
+		GridPane.setHalignment(play, HPos.CENTER);
+		GridPane.setHalignment(scores, HPos.CENTER);
+		GridPane.setHalignment(settings, HPos.CENTER);
+		GridPane.setHalignment(help, HPos.CENTER);
+
 		grid.add(title, 0, 0, 4, 1);
 		grid.add(play, 0, 1);
 		grid.add(scores, 0, 2);
 		grid.add(settings, 0, 3);
 		grid.add(help, 0, 4);
-		
-		Scene scene = new Scene(grid, 200, 200);
+
+		//root.getChildren().addAll(title, play, scores, help);
+		Scene scene = new Scene(grid, 700, 400);
 		stage.setScene(scene);
 		stage.show();
 	}
