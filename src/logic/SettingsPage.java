f983866ef471dfa2590a5b1628aaa7d454aa5983
Brady-pc <Brady-pc@129.65.206.71>
2018-06-04 23:19:18 -0700

    .

---
+++ src/logic/SettingsPage.java
@@ -0,0 +1,28 @@
+package logic;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+
+
+public class SettingsPage {
+	
+	public Scene scene;
+	
+	public SettingsPage(GridPane grid) {
+		scene = new Scene(grid, 700, 400, Color.DIMGRAY);
+	}
+
+}
