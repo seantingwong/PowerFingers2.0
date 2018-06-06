4810767feaaf35c7453567ba4a8f41550bc9496f
Jenna <jsteph11@calpoly.edu>
2018-06-06 16:03:07 -0700

    update score menu

--- src/logic/ScoreMenu.java
+++ src/logic/ScoreMenu.java
@@ -21,18 +21,30 @@ public class ScoreMenu {
 		GridPane grid = new GridPane();
 		final Button prsn = new Button("Personal");
 		final Button top = new Button("Top");
+	
+		prsn.setMaxWidth(200);
+		prsn.setMinWidth(200);
+		top.setMaxWidth(200);
+		top.setMinWidth(200);
 		
         grid.setStyle("-fx-background-color: transparent;");
 		grid.setHgap(5);
 		grid.setVgap(5);
 		grid.setPadding(new Insets(25, 25, 25, 25));
-		grid.getColumnConstraints().add(new ColumnConstraints(100));
+		grid.getColumnConstraints().add(new ColumnConstraints(70));
 		
 		//Page Title
 		Text title = new Text("High Scores");
 		GridPane.setValignment(title, VPos.CENTER);
 		GridPane.setHalignment(title, HPos.CENTER);
 	    title.setStyle("-fx-font: 36 arial;");
+	    title.maxWidth(100);
+	    title.minWidth(100);
+	    
+	    //High Scores
+	    Text subtitle = new Text("Top Scores");
+	    subtitle.setStyle("-fx-font: 20 arial;");
+	    
 		
 	    //Back Button
 		Button back = new Button("Back");
@@ -43,28 +55,31 @@ public class ScoreMenu {
 			}
 		});
 		
-		//Toggle Scores Button
-		top.setStyle("-fx-background-color: LightGrey;");
+		//Toggle Top Scores Button
+		top.setStyle("-fx-background-color: transparent; -fx-font-weight: bold;");
 		top.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
-				top.setText("clicked");
-				prsn.setText("Personal");
+				top.setStyle("-fx-background-color: transparent; -fx-font-weight: bold;");
+				prsn.setStyle("-fx-background-color: LightGrey; -fx-font-weight: bold;");
+				subtitle.setText("Top Scores");
 			}
 		});
 		
 		//Personal Scores Button
-		prsn.setStyle("-fx-background-color: LightGreay);");
+		prsn.setStyle("-fx-background-color: LightGrey;-fx-font-weight: bold;");
 		prsn.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
-				prsn.setText("clicked");
-				top.setText("Top");
+				prsn.setStyle("-fx-background-color: transparent;-fx-font-weight: bold;");
+				top.setStyle("-fx-background-color: LightGrey;-fx-font-weight: bold;");
+				subtitle.setText("Your Scores");
 			}
 		});
 		
 		grid.add(back, 0, 0);
-		grid.add(title, 28, 0);
-		grid.add(top, 28, 20);
-		grid.add(prsn, 29, 20);
+		grid.add(title, 1, 0, 5,1);
+		grid.add(top, 1, 10, 2, 1);
+		grid.add(prsn, 3, 10, 1, 1);
+		grid.add(subtitle, 1, 12);
 		
 		scene = new Scene(grid, StartMenu.height, StartMenu.width, Color.DIMGRAY);
 	}
